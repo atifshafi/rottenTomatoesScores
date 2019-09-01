7b79b4eb15915ffb0adf8dd292776a94f15a6544
Atif <shafiatifbin@gmail.com>
2019-09-01 16:48:30 -0700

    Updaing app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def scores(endpoints, names):
 
         soup = BeautifulSoup(src, features="html.parser")
 
-        #Scraping scores from web
+        # Scraping scores from web
         for link in soup.find_all("span"):
             try:
                 if link.attrs['class'][0] == 'mop-ratings-wrap__percentage':
@@ -42,10 +42,10 @@ def scores(endpoints, names):
             except:
                 pass
 
-        #Calculating total score
+        # Calculating total score
         names[i].append(int(names[i][1]) + int(names[i][2]))
 
-        #Scraping more info of the film from web
+        # Scraping more info of the film from web
         for link in soup.find_all("div"):
             try:
                 if link.attrs['class'][0] == 'meta-value':
@@ -54,11 +54,18 @@ def scores(endpoints, names):
                 pass
 
     # Creating a csv file
-    df = pd.DataFrame(names, columns=['movie_name', 'critic_score', 'audiance_score', 'total_score', 'Rating', 'Genre', 'Directed By', 'Written BY', 'In Theaters', 'On Disc/Streaming', 'Box Office', 'Runtime', 'Studio'])
+    df = pd.DataFrame(names, columns=['movie_name', 'critic_score', 'audiance_score', 'total_score', 'Rating', 'Genre',
+                                      'Directed By', 'Written BY', 'In Theaters', 'On Disc/Streaming', 'Box Office',
+                                      'Runtime', 'Studio'])
     df.to_csv("output.csv", index=False, header=None)
-    fig = px.scatter(df, x="critic_score", y="audiance_score", hover_name="movie_name", hover_data=["Rating", "Genre", "Directed By", "Written BY", "In Theaters", "On Disc/Streaming", "Box Office", "Runtime", "Studio"], color="total_score",
+
+    # Plotting a scatter diagram
+    fig = px.scatter(df, x="critic_score", y="audiance_score", hover_name="movie_name",
+                     hover_data=["Rating", "Genre", "Directed By", "Written BY", "In Theaters", "On Disc/Streaming",
+                                 "Box Office", "Runtime", "Studio"], color="total_score",
                      color_continuous_scale=px.colors.sequential.Viridis, render_mode="webgl")
     fig.show()
+
 
 movie_endpoints()
 scores(movie_endpoints()[0], movie_endpoints()[1])
