4ebc92507a8d7b3a9cccc34e2afcc0dadc7ae0bc
Atif <shafiatifbin@gmail.com>
2019-09-22 22:44:00 -0700

    Adding feature to specify a range for years

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Rotten Tomatoes Top 100 Movies (on a scatter diagram)
 
-This application plots a scatter diagram of audiance score vs critic score for top 100 movies listed on Rotten Tomatoes. Median lines across both axis show disparity between scores and how it can vary on average.
+This application plots a scatter diagram of audiance score vs critic score for top 100 movies listed on Rotten Tomatoes for any set of years. Median lines across both axis show disparity between scores and how it can vary on average.
 
 ## Setup
 
@@ -9,6 +9,8 @@ Please run the requirements.txt file.
 ## Usage
 
 Run app.py script to generate the graph.
+Upon running, user will be asked to enter starting and ending date. Based on the input, a scatter diagram will be plotted for 
+top 100 movies of each year.
 
 ### Snapshot of the graph
-![rotten_tomato_ratings](https://drive.google.com/uc?export=download&id=1dMDnKNwHx1ql9D7gNLodafwHbQevmQ86)
+![rotten_tomato_ratings](https://drive.google.com/uc?export=download&id=17chGT5bqgT53CrNB48IKdmpsi6a8S_uz)
--- app.py
+++ app.py
@@ -7,46 +7,78 @@ import plotly.graph_objs as go
 from numpy import median
 
 
-def movie_endpoints():
-    result = requests.get("https://www.rottentomatoes.com/top/bestofrt/")
+def verify_year(year):
+    while True:
+        if year.isdigit() is True:
+            return year
+        else:
+            print("Please enter numeric values of the year and"
+                  " don't use any spaces\n")
+            year = input("\n")
 
-    src = result.content
 
-    soup = BeautifulSoup(src, features="html.parser")
+def set_year():
+    start_year = verify_year(input("Beginning Year?\n"))
+    end_year = verify_year(input("Ending Year?\n"))
+    while True:
+        if start_year > end_year:
+            print("Ending year can't be earlier than beginning year")
+            end_year = verify_year(input("Ending Year?\n"))
+        else:
+            break
 
+    return int(start_year), int(end_year)
+
+
+def movie_endpoints(start_year, end_year):
     endpoints = []
     names = []
+    for year in range(end_year-start_year + 1):
+        print(year)
+        result = requests.get("https://www.rottentomatoes.com/top/bestofrt/?year=" + str(start_year + year))
 
-    for link in soup.find_all("table"):
-        if link.attrs['class'][0] == 'table':
-            for link in link.find_all("a"):
-                endpoints.append(link.attrs['href'])
-                tmp_list = []
-                tmp_list.append(link.text.lstrip())
-                names.append(tmp_list)
+        src = result.content
 
+        soup = BeautifulSoup(src, features="html.parser")
+
+        for link in soup.find_all("table"):
+            if link.attrs['class'][0] == 'table':
+                for link in link.find_all("a"):
+                    endpoints.append(link.attrs['href'])
+                    tmp_list = []
+                    tmp_list.append(link.text.lstrip())
+                    names.append(tmp_list)
+
+        print(names)
     return endpoints, names
 
 
 def scores(endpoints, names):
     for i in range(len(endpoints)):
-
         result = requests.get("https://www.rottentomatoes.com/" + endpoints[i])
         src = result.content
-
         soup = BeautifulSoup(src, features="html.parser")
 
         # Scraping scores from web
         for link in soup.find_all("span"):
             try:
                 if link.attrs['class'][0] == 'mop-ratings-wrap__percentage':
+                    #print(link.text)
                     names[i].append(re.sub("[^0-9]", "", link.text))
             except:
                 pass
 
+        # Removing any movie from the list which doesn't have any audiance score
+        try:
+            if len(names[i]) < 3:
+                del names[i]
+                continue
+        except:
+            break
+
         # Calculating total score
         names[i].append(int(names[i][1]) + int(names[i][2]))
-
+        print(names[i])
         # Scraping more info of the film from web
         for link in soup.find_all("div"):
             try:
@@ -87,5 +119,6 @@ def scores(endpoints, names):
     fig.show()
 
 
-movie_endpoints()
-scores(movie_endpoints()[0], movie_endpoints()[1])
+start, end = set_year()
+scores(movie_endpoints(start, end)[0],
+       movie_endpoints(start, end)[1])
